a327e91e06b5e58c7da1b6cfba52ead649adaa58
srathbun13 <shauna.rathbun@eagles.oc.edu>
2020-09-25 09:46:28 -0500

    Updated courselist page

--- src/components/courselist.vue
+++ src/components/courselist.vue
@@ -5,6 +5,14 @@
   <span>Edit </span>
   <span> Delete</span>
   <span>)</span>
+  <table border='1' width='70%' style='border-collapse: collapse;'>
+    <tr>
+      <th>Course Number</th>
+      <th>Course Name</th>
+    </tr>
+  
+  </table>
+
 </div>
 </template>
 
