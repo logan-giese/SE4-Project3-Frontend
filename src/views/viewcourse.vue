af3e15d3a66ceecae5e3d5a1427d52742c77320b
SethKeller <seth.m.keller@eagles.oc.edu>
2020-09-30 21:29:38 -0500

    Accidentally made commits to the wrong branch, not pushing those commits to the repo. Moved changes to the correct branch, and pushing.
    Added the view couse page and routing features.   Page does not yet show data.

---
+++ src/views/viewcourse.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+    <h1>MyCourses - Course List</h1>
+    <h3>{{ message }}</h3>
+</div>    
+</template>
+
+<script>
+import CourseServices from '@/services/courseservices.js'
+export default {
+    props:{MyID: Number},
+    data(){
+        return {
+            message: "Loading..."
+        }
+
+    },
+    methods: {
+        getCourse: function(){
+           return CourseServices.getCourse(this.MyID)
+        }
+    },
+    created()
+    {
+        this.getCourse(this.MyID)
+    }
+}
+</script>
+
+<style>
+    table {
+        margin: auto;
+    }
+    td {
+        border-bottom: solid 1px rgba(0,0,0,0.35);
+    }
+    .list-item {
+        text-align: left;
+    }
+    .list-header {
+        font-weight: bold;
+    }
+</style>
