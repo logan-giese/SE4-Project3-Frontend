ee57971b33b069b55c09a82bfccbd687c0a8fb33
srathbun13 <shauna.rathbun@eagles.oc.edu>
2020-10-12 16:54:56 -0500

    Added Title fields

---
+++ src/components/DegreelistDisplay.vue
@@ -0,0 +1,29 @@
+<template>
+<tr class="list-item">
+    <td>{{ degree.number }}</td>
+    <td>{{ degree.name }}</td>
+    <td>
+        <router-link :to="{ name: 'view', params: { id: degree.id } }">
+            <span>View</span>
+        </router-link>
+        <span> | </span>
+        <router-link :to="{ name: 'edit', params: { id: degree.id } }">
+            <span>Edit</span>
+        </router-link>
+        <span> | </span>
+        <router-link :to="{ name: 'delete', params: { id: degree.id } }">
+            <span>Delete</span>
+        </router-link>
+    </td>
+</tr>
+</template>
+
+<script>
+    export default {
+        props: {
+            degree: Object
+        }
+    }
+</script>
+
+<style></style>
