bfa6fddc678ffc5cfee205e717ee87d819763412
srathbun13 <shauna.rathbun@eagles.oc.edu>
2020-10-19 16:51:43 -0500

    Updated services for CourseEdit

--- src/components/courseadd.vue
+++ src/components/courseadd.vue
@@ -1,38 +1,92 @@
 <template>
-<div class ='field'>
-    <tr>
-        <td>
-            <input type="text" v-model="course.name">
-        </td>
-
-        <td>
-            <input type="text" v-model="course.description">
-        </td>
-        <td>
-            <input type="text" v-model="course.number">
-        </td>
-        <td>
-            <button @click="addCourse">Save</button>
-        </td>
-        </tr>
-</div>
+    <div class="submit-form">
+        <div v-if="!submitted">
+                <div class="form-group">
+                <label for="number">Number </label>
+                <input
+                class="form-control"
+                id="description"
+                required
+                v-model="course.number"
+                name="number"
+                />
+            </div>
+            <div class="form-group">
+                <label for="name">Name </label>
+                <input
+                type="text"
+                class="form-control"
+                id="name"
+                required
+                v-model="course.name"
+                name="name"
+                />
+            </div>
+            
+            <div class="form-group">
+                <label for="description">Description </label>
+                <input
+                class="form-control"
+                id="description"
+                required
+                v-model="course.description"
+                name="description"
+                />
+            </div>
+        
+            <button @click="saveCourse" class="btn btn-success">Save</button>
+        </div>
+        <div v-else>
+            <h4>Submission complete</h4>
+            <button class="btn btn-sucess" @click="newCourse">Add</button>
+        </div>
+    </div>
 </template>
 
 <script>
+import courseservices from "../services/courseservices";
 export default {
-    data:function(){
+    name: "add-course",
+    data(){
         return{
-            course:{}
-        }
+            course:{
+                id:null,
+                title: "",
+                description: "",
+                published: false
+            },
+            submitted:false
+        };
     },
     methods: {
-        enableEdit: function(){
-            this.editForm=true
+        saveCourse(){
+            var data ={
+                title: this.course.title,
+                description: this.course.description
+            };
+            courseservices.create(data)
+            .then(response=>{
+                this.course.id=response.data.id;
+                console.log(response.data);
+                this.submitted=true;
+            })
+            .catch(e=>{
+                console.log(e);
+
+            });
+
         },
-        addCourse: function() {
-            this.$emit("addCourse", this.course)
+        newCourse(){
+            this.submitted=false;
+            this.course={};
         }
     }
     
-}
+};
 </script>
+<style scoped>
+.submit-form{
+    max-width:300px;
+    margin:auto;
+}
+</style>
