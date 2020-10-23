d5d96899306ca98c8936e3c356b3f63aa7b83810
srathbun13 <shauna.rathbun@eagles.oc.edu>
2020-10-23 09:37:43 -0500

    Updated Cancel Button

---
+++ src/components/DegreeAdd.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="submit-form">
+        <div v-if="!submitted">
+                <div class="form-group">
+                <label for="number"> Number </label>
+                <input
+                class="form-control"
+                id="description"
+                required
+                v-model="degree.number"
+                name="number"
+                />
+            </div>
+            <div class="form-group">
+                <label for="name"> Name </label>
+                <input
+                type="text"
+                class="form-control"
+                id="name"
+                required
+                v-model="degree.name"
+                name="name"
+                />
+            </div>
+            <div class="form-group">
+                <label for="department"> Department </label>
+                <input
+                class="form-control"
+                id="department"
+                required
+                v-model="degree.department"
+                name="department"
+                />
+            </div>
+            <button @click="saveDegree" class="btn btn-success">Save</button>
+        </div>
+        <div v-else>
+            <h4>Submission complete</h4>
+            <button class="btn btn-sucess" @click="newDegree">Add</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import courseservices from "../services/courseservices";
+export default {
+    name: "add-degree",
+    data(){
+        return{
+            degree:{
+                id:null,
+            },
+            submitted:false
+        };
+    },
+    methods: {
+        saveDegree(){
+            var data ={
+                name: this.course.name,
+
+
+            };
+            courseservices.addDegree(data)
+            .then(response=>{
+                this.degree.id=response.data.id;
+                console.log(response.data);
+                this.submitted=true;
+            })
+            .catch(e=>{
+                console.log(e);
+
+            });
+
+        },
+        newCourse(){
+            this.submitted=false;
+            this.degree={};
+        }
+    }
+    
+};
+</script>
+<style scoped>
+.submit-form{
+    max-width:300px;
+    margin:auto;
+}
+</style>
