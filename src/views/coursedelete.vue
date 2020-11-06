086153183437c33f94702ac6667e27cc43e9059c
logan-giese <logan.giese@eagles.oc.edu>
2020-11-06 12:15:16 -0600

    Improve return behavior after deleting course
    Now properly returns to the course list after deleting from the view course page

--- src/views/coursedelete.vue
+++ src/views/coursedelete.vue
@@ -28,14 +28,26 @@ export default {
                 this.message = error.response.data.message
             })
         },
-        deleteCourse: function(){
+        deleteCourse: function() {
             CourseServices.deleteCourse(this.id)
-            this.$router.go(-1)
+            setTimeout(() => {
+                // If the user came from the view course page, go back twice (to get to the course list)
+                if (this.prevRoute.name == "view-course")
+                    this.$router.go(-2)
+                else
+                    this.$router.go(-1)
+            }, 150) // Small delay to allow DB to process deletion
         }
     },
     created()
     {
         this.getCourse()
+    },
+    // Set up local variable to identify which page the user came from (accessed by this.prevRoute)
+    beforeRouteEnter(to, from, next) {
+        next(vm => {
+            vm.prevRoute = from
+        })
     }
 }
 </script>
