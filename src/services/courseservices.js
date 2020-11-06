82d887ac8cba03b7af104498f0dd6e911012aa44
logan-giese <logan.giese@eagles.oc.edu>
2020-11-06 14:04:07 -0600

    Add course API services for students, advisors, and plans

--- src/services/courseservices.js
+++ src/services/courseservices.js
@@ -23,6 +23,7 @@ const apiClient = axios.create({
 })
 
 export default {
+    // Course requests:
     getCourses(page = 1, searchQuery) {
         var perPage = 80; // Number of results per page
         var search = searchQuery != "" ? "&search="+searchQuery : "";
@@ -39,5 +40,63 @@ export default {
     },
     deleteCourse(id) {
         return apiClient.delete("courses/" + id);
+    },
+    
+    // Student requests:
+    getStudents(page = 1, searchQuery) {
+        var perPage = 80; // Number of results per page
+        var search = searchQuery != "" ? "&search="+searchQuery : "";
+        return apiClient.get("students?per_page="+perPage+"&page=" + (parseInt(page)-1)*perPage + search); // Page to offset
+    },
+    getStudent(id) {
+        return apiClient.get("students/" + id);
+    },
+    updateStudent(id, student) {
+        return apiClient.put("students/" + id, student);
+    },
+    addStudent(student) {
+        return apiClient.post("students", student);
+    },
+    deleteStudent(id) {
+        return apiClient.delete("students/" + id);
+    },
+
+    // Advisor requests:
+    getAdvisors(page = 1, searchQuery) {
+        var perPage = 80; // Number of results per page
+        var search = searchQuery != "" ? "&search="+searchQuery : "";
+        return apiClient.get("advisors?per_page="+perPage+"&page=" + (parseInt(page)-1)*perPage + search); // Page to offset
+    },
+    getAdvisor(id) {
+        return apiClient.get("advisors/" + id);
+    },
+    updateAdvisor(id, advisor) {
+        return apiClient.put("advisors/" + id, advisor);
+    },
+    addAdvisor(advisor) {
+        return apiClient.post("advisors", advisor);
+    },
+    deleteAdvisor(id) {
+        return apiClient.delete("advisors/" + id);
+    },
+    
+    // Degree plan requests:
+    getPlans(page = 1, searchQuery) {
+        var perPage = 80; // Number of results per page
+        var search = searchQuery != "" ? "&search="+searchQuery : "";
+        return apiClient.get("plans?per_page="+perPage+"&page=" + (parseInt(page)-1)*perPage + search); // Page to offset
+    },
+    getPlan(id) {
+        // NOTE: Returns a collection of the courses in the specified plan (degreeplans natural joined with courseplan)
+        return apiClient.get("plans/" + id);
+    },
+    updatePlan(id, plan) {
+        return apiClient.put("plans/" + id, plan);
+    },
+    addPlan(plan) {
+        return apiClient.post("plans", plan);
+    },
+    deletePlan(id) {
+        return apiClient.delete("plans/" + id);
     }
 }
